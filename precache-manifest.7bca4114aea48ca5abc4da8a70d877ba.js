dd4d1f7435d5c65f9c4678554e5b7a9cef8b8bd3
Andrei Seceleanu <aseceleanu@totalsoft.ro>
2019-12-18 16:43:01 +0200

    Updates

---
+++ precache-manifest.7bca4114aea48ca5abc4da8a70d877ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c355f8c9a42f07269c5e5124286ca85",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "5867ca43da1051bd45f3",
+    "url": "/myapp/static/css/main.bbf6d9b8.chunk.css"
+  },
+  {
+    "revision": "2fea7a95dfe82810cb5c",
+    "url": "/myapp/static/js/2.93740813.chunk.js"
+  },
+  {
+    "revision": "5867ca43da1051bd45f3",
+    "url": "/myapp/static/js/main.b36e50f9.chunk.js"
+  },
+  {
+    "revision": "582c1416178d8555d0ce",
+    "url": "/myapp/static/js/runtime-main.1771ca49.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/myapp/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/myapp/static/media/shopping-bag.30f07071.svg"
+  }
+]);
